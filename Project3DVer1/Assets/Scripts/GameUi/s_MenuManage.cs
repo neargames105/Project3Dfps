3c79a37707144d68273a8b2ec27b3a93560bf312
neargames105 <68529349+neargames105@users.noreply.github.com>
2021-09-06 17:02:13 +0700

    update_menuui

---
+++ Project3DVer1/Assets/Scripts/GameUi/s_MenuManage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class s_MenuManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("LevelIndex", 1));
+    }
+}
